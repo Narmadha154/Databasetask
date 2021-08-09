628118b6738d7cba3f644b92bca39663cb15d77f
inc11 <154narmadhas@gmail.com>
2021-08-09 17:37:24 +0530

    first commit

--- src/dbtask/DetailsList.java
+++ src/dbtask/DetailsList.java
@@ -3,13 +3,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DetailsList {
-    public static ArrayList<Customer_Info> getCustomerInfo() {
-        ArrayList<Customer_Info> customer = new ArrayList<>();
-        Customer_Info obj = new Customer_Info();
-        /*int customerId=134;*/
+    public static ArrayList<CustomerInfo> getCustomerInfo() {
+        ArrayList<CustomerInfo> customer = new ArrayList<>();
+        CustomerInfo obj = new CustomerInfo();
         Scanner sc = new Scanner(System.in);
         Scanner sc1 = new Scanner(System.in);
-       /* customerId=customerId+1;*/
         System.out.println("Enter your name:");
         String name = sc1.nextLine();
         System.out.println("Enter your mobile number:");
@@ -20,13 +18,12 @@ public class DetailsList {
         obj.setMobile(mobile);
         obj.setAge(age);
         customer.add(obj);
-        /*System.out.println("Your customer Id is:" + customerId);*/
         return customer;
     }
 
-    public static ArrayList<Account_Info> getAccountInfo(){
-        ArrayList<Account_Info> account=new ArrayList<>();
-        Account_Info obj1=new Account_Info();
+    public static ArrayList<AccountInfo> getAccountInfo(){
+        ArrayList<AccountInfo> account=new ArrayList<>();
+        AccountInfo obj1=new AccountInfo();
         Scanner sc=new Scanner(System.in);
         Scanner sc1=new Scanner(System.in);
         System.out.println("Enter your customer id:");
