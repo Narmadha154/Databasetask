06ee40881d6b2623e2ff3c94eeb5c70ff7252025
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-08-09 21:44:35 +0530

    Update InsertNewCustomer.java

--- src/dbhandler/InsertNewCustomer.java
+++ src/dbhandler/InsertNewCustomer.java
@@ -4,13 +4,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class InsertNewCustomer {
+public class InsertNewCustomer<object> {
     public static void insertInfo() {
        /* Connection con = ConnectionDb.createConnection();*/
         try (PreparedStatement pst = con.prepareStatement("Insert into CustomerInfo(Name,Mobile,Age)" + "values (?,?,?)")) {
-            pst.setString(1,name);
-            pst.setString(2, mobile);
-            pst.setInt(3, age);
+            CustomerInfo[] customer=new CustomerInfo[0];
+            for(CustomerInfo cus:customer){
+            pst.setString(1,cus.getName());
+            pst.setString(2,cus.getMobile());
+            pst.setInt(3,cus.getAge());
+            }
             int row = pst.executeUpdate();
             System.out.println(row);
         } catch (SQLException throwable) {
@@ -27,9 +30,12 @@ public class InsertNewCustomer {
             throwable.printStackTrace();
         }*/
         try (PreparedStatement pst = con.prepareStatement("Insert into AccountInfo(CusId,Branch,Balance)" + " values (?,?,?)")) {
-            pst.setInt(0, cusId);
-            pst.setString(2, branch);
-            pst.setDouble(3, balance);
+            AccountInfo[] account=new AccountInfo[0];
+            for(AccountInfo accinfo:account){
+            pst.setInt(0,accinfo.getCusId());
+            pst.setString(2,accinfo.getBranch());
+            pst.setDouble(3,accinfo.getBalance);
+            }
             pst.executeUpdate();
         } catch (SQLException throwable) {
             throwable.printStackTrace();
